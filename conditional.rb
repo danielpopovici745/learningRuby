86b3168cf179275f36608f81d60d126bea571aff
Daniel Popovici <dpopovici601@gmail.com>
2022-09-14 15:40:28 -0400

    more practice with conditionals

--- conditional.rb
+++ conditional.rb
@@ -1,5 +1,5 @@
-puts "Put in a number"
-a = gets.chomp.to_i
+# puts "Put in a number"
+# a = gets.chomp.to_i
 
 # if a == 3
 #   puts "a is 3"
@@ -9,4 +9,13 @@ a = gets.chomp.to_i
 #   puts "a is neither 3, nor 4"
 # end
 
-(a != 3) && (a != 4) ? (puts "your number is not 3 or 4") : (puts "your number is #{a}")
+# (a != 3) && (a != 4) ? (puts "your number is not 3 or 4") : (puts "your number is #{a}")
+
+# any expression will evaluate to true except false and nil
+
+a = "false"
+if a
+  puts "how can this be true?"
+else
+  puts "it is not true"
+end
